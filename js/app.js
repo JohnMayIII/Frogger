0df25f50372bb44826a967e59dd599cce3bc1177
John May <john@mscodingacademies.org>
2023-01-27 15:04:19 -0600

    added collision with cars

--- js/app.js
+++ js/app.js
@@ -28,14 +28,19 @@ class Frogger{
 
         this.frogColor = 'green'
         this.frogBorder = 'green'
-        this.frog = {x:300,y:600}
+        this.frog = {x:300,y:600,hw:0,w:0}
         
-        this.LogColor = 'brown'
-        this.LogBorder = 'rgb(129, 58, 27)'
+        this.LogColor = 'rgb(129, 58, 27)'
+        this.LogBorder = 'burlywood'
 
         this.topLog = {x:600, y:50, w:0}
         this.topLog2 = {x:900, y:50, w:0}
         this.topLog3 = {x:1200, y:50, w:0}
+
+        this.scndTopLog = {x:-150,y:100,w:0}
+        this.scndTopLog2 = {x:-450,y:100,w:0}
+        this.scndTopLog3 = {x:-750,y:100,w:0}
+        
 
         this.scndBtmLog = {x:600, y:150, w:0}
         this.scndBtmLog2 = {x:900, y:150, w:0}
@@ -45,6 +50,11 @@ class Frogger{
         this.btmLog2 = {x:900, y:200, w:0}
         this.btmLog3 = {x:1200, y:200, w:0}
 
+        this.trueBtmLog = {x:-150,y:250,w:0}
+        this.trueBtmLog2 = {x:-450,y:250,w:0}
+        this.trueBtmLog3 = {x:-750,y:250,w:0}
+        
+
         this.busColor = 'yellow'
         this.busBorder = 'yellow'
 
@@ -52,8 +62,8 @@ class Frogger{
         this.bus2 = {x:1000,y:350,w:0}
         this.bus3 = {x:1400,y:350,w:0}
 
-        this.fastCarColor = 'red'
-        this.fastCarBorder = 'red'
+        this.fastCarColor = 'maroon'
+        this.fastCarBorder = 'maroon'
 
         this.fastCar = {x:-50,y:400,w:0}
         this.fastCar2 = {x:-650,y:400,w:0}
@@ -71,6 +81,20 @@ class Frogger{
         this.dumptruck = {x:-50,y:500,w:0}
         this.dumptruck2 = {x:-350,y:500,w:0}
         this.dumptruck3 = {x:-650,y:500,w:0}
+
+        this.bottomCarColor = 'red'
+        this.bottomCarBorder = 'red'
+
+        this.bottomCar = {x:600,y:550,w:0}
+        this.bottomCar2 = {x:850,y:550,w:0}
+        this.bottomCar3 = {x:1100,y:550,w:0}
+
+        this.goalZoneColor = "rgb(7, 7, 87)"
+        this.goalZoneBorder = "lightgreen"
+
+        this.goalZone = [
+            {x:50,y:0,w:0}, {x:165,y:0,w:0}, {x:280,y:0,w:0}, {x:390,y:0,w:0}, {x:500,y:0,w:0}
+        ] 
 
         this.lives = 6
     }
@@ -113,6 +137,7 @@ class Frogger{
         const sideWalk = this.sideWalk
         const grassBed = this.grassBed
         const river = this.river
+        const goalZone = this.goalZone
 
         sideWalk.forEach(part =>{
             context.fillStyle = this.sideWalkColor
@@ -120,12 +145,20 @@ class Frogger{
             context.fillRect(part.x, part.y, 600, 50)
             context.strokeRect(part.x, part.y, 600, 50)
         })
-
+        
         grassBed.forEach(part =>{
             context.fillStyle = this.grassBedColor
             context.strokeStyle = this.grassBedBorder
             context.fillRect(part.x, part.y, 600, 50)
             context.strokeRect(part.x, part.y, 600, 50)
+        })
+
+        goalZone.forEach(part =>{
+            context.fillStyle = this.goalZoneColor
+            context.strokeStyle = this.goalZoneBorder
+            context.fillRect(part.x, part.y, 50, 50)
+            context.strokeRect(part.x, part.y, 50, 50)
+            part.w = part.x + 50
         })
 
         river.forEach(part =>{
@@ -145,6 +178,7 @@ class Frogger{
         context.fillRect(frog.x, frog.y, 50, 50)
         context.strokeRect(frog.x, frog.y, 50, 50)
         this.frog.w = this.frog.x + 50
+        this.frog.hw = this.frog.x + 25
     }
 
     onkeydown(event) {
@@ -174,12 +208,6 @@ class Frogger{
         if (frog.y < 0){
             frog.y = 0
         }
-        // if(frog.x < 0){
-        //     frog.x = 0
-        // }  
-        // if(frog.x >= 600){
-        //     frog.x = 550
-        // }
     }
 
     spawnLogs(){
@@ -188,6 +216,10 @@ class Frogger{
         const topLog = this.topLog
         const topLog2 = this.topLog2
         const topLog3 = this.topLog3
+
+        const scndTopLog = this.scndTopLog
+        const scndTopLog2 = this.scndTopLog2
+        const scndTopLog3 = this.scndTopLog3
         
         const scndBtmLog = this.scndBtmLog
         const scndBtmLog2 = this.scndBtmLog2
@@ -196,6 +228,11 @@ class Frogger{
         const btmLog = this.btmLog
         const btmLog2 = this.btmLog2
         const btmLog3 = this.btmLog3
+
+        const trueBtmLog = this.trueBtmLog
+        const trueBtmLog2 = this.trueBtmLog2
+        const trueBtmLog3 = this.trueBtmLog3
+        
         
         context.fillStyle = this.LogColor
         context.strokeStyle = this.LogBorder
@@ -214,6 +251,24 @@ class Frogger{
         context.fillRect(topLog3.x, topLog3.y, 150, 50)
         context.strokeRect(topLog3.x, topLog3.y, 150, 50)
         this.topLog3.w = this.topLog3.x + 150
+
+        context.fillStyle = this.LogColor
+        context.strokeStyle = this.LogBorder
+        context.fillRect(scndTopLog.x,scndTopLog.y, 150, 50)
+        context.strokeRect(scndTopLog.x,scndTopLog.y, 150, 50)
+        this.scndTopLog.w = this.scndTopLog.x + 150
+
+        context.fillStyle = this.LogColor
+        context.strokeStyle = this.LogBorder
+        context.fillRect(scndTopLog2.x, scndTopLog2.y, 150, 50)
+        context.strokeRect(scndTopLog2.x, scndTopLog2.y, 150, 50)
+        this.scndTopLog2.w = this.scndTopLog2.x + 150
+
+        context.fillStyle = this.LogColor
+        context.strokeStyle = this.LogBorder
+        context.fillRect(scndTopLog3.x, scndTopLog3.y, 150, 50)
+        context.strokeRect(scndTopLog3.x, scndTopLog3.y, 150, 50)
+        this.scndTopLog3.w = this.scndTopLog3.x + 150
         
         context.fillStyle = this.LogColor
         context.strokeStyle = this.LogBorder
@@ -250,6 +305,24 @@ class Frogger{
         context.fillRect(btmLog3.x, btmLog3.y, 150, 50)
         context.strokeRect(btmLog3.x, btmLog3.y, 150, 50)
         this.btmLog3.w = this.btmLog3.x + 150
+
+        context.fillStyle = this.LogColor
+        context.strokeStyle = this.LogBorder
+        context.fillRect(trueBtmLog.x, trueBtmLog.y, 150, 50)
+        context.strokeRect(trueBtmLog.x, trueBtmLog.y, 150,50)
+        this.trueBtmLog.w = this.trueBtmLog.x + 150
+
+        context.fillStyle = this.LogColor
+        context.strokeStyle = this.LogBorder
+        context.fillRect(trueBtmLog2.x, trueBtmLog2.y, 150, 50)
+        context.strokeRect(trueBtmLog2.x, trueBtmLog2.y, 150,50)
+        this.trueBtmLog2.w = this.trueBtmLog2.x + 150
+
+        context.fillStyle = this.LogColor
+        context.strokeStyle = this.LogBorder
+        context.fillRect(trueBtmLog3.x, trueBtmLog3.y, 150, 50)
+        context.strokeRect(trueBtmLog3.x, trueBtmLog3.y, 150,50)
+        this.trueBtmLog3.w = this.trueBtmLog3.x + 150
     }
     
     spawnCars(){
@@ -268,7 +341,11 @@ class Frogger{
 
         const dumptruck = this.dumptruck
         const dumptruck2 = this.dumptruck2
-        const dumptruck3= this.dumptruck3
+        const dumptruck3 = this.dumptruck3
+
+        const bottomCar = this.bottomCar
+        const bottomCar2 = this.bottomCar2
+        const bottomCar3 = this.bottomCar3
         
         context.fillStyle = this.busColor
         context.strokeStyle = this.busBorder
@@ -335,6 +412,24 @@ class Frogger{
         context.fillRect(dumptruck3.x, dumptruck3.y, 50, 50)
         context.strokeRect(dumptruck3.x, dumptruck3.y, 50, 50)
         this.dumptruck3.w = this.dumptruck3.x + 50
+
+        context.fillStyle = this.bottomCarColor
+        context.strokeStyle = this.bottomCarBorder
+        context.fillRect(bottomCar.x, bottomCar.y, 50, 50)
+        context.strokeRect(bottomCar.x, bottomCar.y, 50, 50)
+        this.bottomCar.w = this.bottomCar.x + 50
+
+        context.fillStyle = this.bottomCarColor
+        context.strokeStyle = this.bottomCarBorder
+        context.fillRect(bottomCar2.x, bottomCar2.y, 50, 50)
+        context.strokeRect(bottomCar2.x, bottomCar2.y, 50, 50)
+        this.bottomCar2.w = this.bottomCar2.x + 50
+
+        context.fillStyle = this.bottomCarColor
+        context.strokeStyle = this.bottomCarBorder
+        context.fillRect(bottomCar3.x, bottomCar3.y, 50, 50)
+        context.strokeRect(bottomCar3.x, bottomCar3.y, 50, 50)
+        this.bottomCar3.w = this.bottomCar3.x + 50
     }
 
     moveCars(){
@@ -351,7 +446,11 @@ class Frogger{
 
         const dumptruck = this.dumptruck
         const dumptruck2 = this.dumptruck2
-        const dumptruck3= this.dumptruck3
+        const dumptruck3 = this.dumptruck3
+
+        const bottomCar = this.bottomCar
+        const bottomCar2 = this.bottomCar2
+        const bottomCar3 = this.bottomCar3
 
         requestAnimationFrame(this.moveLogs)
 
@@ -373,6 +472,11 @@ class Frogger{
         dumptruck.x += dumptruckSpeed
         dumptruck2.x += dumptruckSpeed
         dumptruck3.x += dumptruckSpeed
+
+        const bottomCarSpeed = .5
+        bottomCar.x -= bottomCarSpeed
+        bottomCar2.x -= bottomCarSpeed
+        bottomCar3.x -= bottomCarSpeed
     }
 
     respawnCars(){
@@ -390,6 +494,10 @@ class Frogger{
         const dumptruck = this.dumptruck
         const dumptruck2 = this.dumptruck2
         const dumptruck3= this.dumptruck3
+
+        const bottomCar = this.bottomCar
+        const bottomCar2 = this.bottomCar2
+        const bottomCar3 = this.bottomCar3
 
         if (bus.x <= -600){
             bus.x = 600
@@ -427,12 +535,27 @@ class Frogger{
         if (dumptruck3.x >= 600){
             dumptruck3.x = -250
         }
+
+        if (bottomCar.x <= -150){
+            bottomCar.x = 600
+        }
+        if(bottomCar2.x <= -150){
+            bottomCar2.x = 600
+        }
+        if(bottomCar3.x <= -150){
+            bottomCar3.x = 600
+        } 
     }
 
     moveLogs(){
         const topLog = this.topLog
+        console.log(topLog)
         const topLog2 = this.topLog2
         const topLog3 = this.topLog3
+
+        const scndTopLog = this.scndTopLog
+        const scndTopLog2 = this.scndTopLog2
+        const scndTopLog3 = this.scndTopLog3
         
         const scndBtmLog = this.scndBtmLog
         const scndBtmLog2 = this.scndBtmLog2
@@ -441,12 +564,21 @@ class Frogger{
         const btmLog = this.btmLog
         const btmLog2 = this.btmLog2
         const btmLog3 = this.btmLog3
+
+        const trueBtmLog = this.trueBtmLog
+        const trueBtmLog2 = this.trueBtmLog2
+        const trueBtmLog3 = this.trueBtmLog3
         requestAnimationFrame(this.moveLogs)
 
         const topLogSpeed = .5
         topLog.x -= topLogSpeed
         topLog2.x -= topLogSpeed
         topLog3.x -= topLogSpeed
+
+        const scndTopLogSpeed = .75
+        scndTopLog.x += scndTopLogSpeed
+        scndTopLog2.x += scndTopLogSpeed
+        scndTopLog3.x += scndTopLogSpeed
 
         const scndBtmLogSpeed = .25
         scndBtmLog.x -= scndBtmLogSpeed
@@ -457,6 +589,11 @@ class Frogger{
         btmLog.x -= btmLogSpeed
         btmLog2.x -= btmLogSpeed
         btmLog3.x -= btmLogSpeed
+
+        const trueBtmLogSpeed = .6
+        trueBtmLog.x += trueBtmLogSpeed
+        trueBtmLog2.x += trueBtmLogSpeed
+        trueBtmLog3.x += trueBtmLogSpeed
     }
 
     rideLog(){
@@ -476,13 +613,27 @@ class Frogger{
             }
         }
 
+        if(frog.y == 100){
+            if(frog.x >= this.scndTopLog.x && frog.w <= this.scndTopLog.w){
+                frog.x += .75
+            } else if(frog.x >= this.scndTopLog2.x && frog.w <= this.scndTopLog2.w){
+                frog.x += .75
+            } else if(frog.x >= this.scndTopLog3.x && frog.w <= this.scndTopLog3.w){
+                frog.x += .75
+            } else{
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
+
         if(frog.y == 150){
             if(frog.x >= this.scndBtmLog.x && frog.w <= this.scndBtmLog.w){
-                frog.x -= .3
+                frog.x -= .25
             } else if(frog.x >= this.scndBtmLog2.x && frog.w <= this.scndBtmLog2.w){
-                frog.x -= .3
+                frog.x -= .25
             } else if(frog.x >= this.scndBtmLog3.x && frog.w <= this.scndBtmLog3.w){
-                frog.x -= .3
+                frog.x -= .25
             } else{
                 this.lives--
                 frog.x = 300
@@ -492,11 +643,25 @@ class Frogger{
 
         if(frog.y == 200){
             if(frog.x >= this.btmLog.x && frog.w <= this.btmLog.w){
-                frog.x += .4
+                frog.x -= .4
             } else if(frog.x >= this.btmLog2.x && frog.w <= this.btmLog2.w){
-                frog.x += .4
+                frog.x -= .4
             } else if(frog.x >= this.btmLog3.x && frog.w <= this.btmLog3.w){
-                frog.x += .4
+                frog.x -= .4
+            } else{
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
+
+        if(frog.y == 250){
+            if(frog.x >= this.trueBtmLog.x && frog.w <= this.trueBtmLog.w){
+                frog.x += .6
+            } else if(frog.x >= this.trueBtmLog2.x && frog.w <= this.trueBtmLog2.w){
+                frog.x += .6
+            } else if(frog.x >= this.trueBtmLog3.x && frog.w <= this.trueBtmLog3.w){
+                frog.x += .6
             } else{
                 this.lives--
                 frog.x = 300
@@ -510,13 +675,21 @@ class Frogger{
         const topLog2 = this.topLog2
         const topLog3 = this.topLog3
 
+        const scndTopLog = this.scndTopLog
+        const scndTopLog2 = this.scndTopLog2
+        const scndTopLog3 = this.scndTopLog3
+
         const scndBtmLog = this.scndBtmLog
         const scndBtmLog2 = this.scndBtmLog2
         const scndBtmLog3 = this.scndBtmLog3
 
         const btmLog = this.btmLog
         const btmLog2 = this.btmLog2
-        const btmLog3 =this.btmLog3
+        const btmLog3 = this.btmLog3
+
+        const trueBtmLog = this.trueBtmLog
+        const trueBtmLog2 = this.trueBtmLog2
+        const trueBtmLog3 = this.trueBtmLog3
 
         if (topLog.x <= -300){
             topLog.x = 600
@@ -526,6 +699,16 @@ class Frogger{
         }
         if(topLog3.x <= -300){
             topLog3.x = 600
+        }
+
+        if (scndTopLog.x >= 600){
+            scndTopLog.x = -300
+        }
+        if(scndTopLog2.x >= 600){
+            scndTopLog2.x = -300
+        }
+        if(scndTopLog3.x >= 600){
+            scndTopLog3.x = -300
         }
 
         if(scndBtmLog.x <= -300){
@@ -547,6 +730,41 @@ class Frogger{
         if(btmLog3.x <= -300){
             btmLog3.x = 600
         }
+
+        if (trueBtmLog.x >= 600){
+            trueBtmLog.x = -300
+        }
+        if(trueBtmLog2.x >= 600){
+            trueBtmLog2.x = -300
+        }
+        if(trueBtmLog3.x >= 600){
+            trueBtmLog3.x = -300
+        }
+    }
+
+    frogGoal(){
+        const frog = this.frog
+        const goalZone = this.goalZone
+
+        if(frog.y == 0){
+            goalZone.forEach(part =>{
+                if(frog.hw >= part.x && frog.hw <= part.w){
+                    frog.x = 300
+                    frog.y = 600
+
+                    context.fillStyle = this.frogColor
+                    context.strokeStyle = this.riverColor
+                    context.fillRect(part.x, part.y, 50, 50)
+                    context.strokeRect(part.x, part.y, 50, 50)
+                }else{
+                    this.lives--
+                    frog.x = 300
+                    frog.y = 600
+                }
+            })
+        }
+
+
     }
 
     frogDeath(){
@@ -560,6 +778,90 @@ class Frogger{
         }
 
         //frog hits car
+        if(frog.y == 350){
+            if((frog.x <= this.bus.w && frog.x >= this.bus.x) || (frog.w <= this.bus.w && frog.w >= this.bus.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.bus2.w && frog.x >= this.bus2.x) || (frog.w <= this.bus2.w && frog.w >= this.bus2.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.bus3.w && frog.x >= this.bus3.x) || (frog.w <= this.bus3.w && frog.w >= this.bus3.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
+
+        if(frog.y == 400){
+            if((frog.x <= this.fastCar.w && frog.x >= this.fastCar.x) || (frog.w <= this.fastCar.w && frog.w >= this.fastCar.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.fastCar2.w && frog.x >= this.fastCar2.x) || (frog.w <= this.fastCar2.w && frog.w >= this.fastCar2.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
+
+        if(frog.y == 450){
+            if((frog.x <= this.greenCar.w && frog.x >= this.greenCar.x) || (frog.w <= this.greenCar.w && frog.w >= this.greenCar.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.greenCar2.w && frog.x >= this.greenCar2.x) || (frog.w <= this.greenCar2.w && frog.w >= this.greenCar2.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.greenCar3.w && frog.x >= this.greenCar3.x) || (frog.w <= this.greenCar3.w && frog.w >= this.greenCar3.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
+
+        if(frog.y == 500){
+            if((frog.x <= this.dumptruck.w && frog.x >= this.dumptruck.x) || (frog.w <= this.dumptruck.w && frog.w >= this.dumptruck.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.dumptruck2.w && frog.x >= this.dumptruck2.x) || (frog.w <= this.dumptruck2.w && frog.w >= this.dumptruck2.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.dumptruck3.w && frog.x >= this.dumptruck3.x) || (frog.w <= this.dumptruck3.w && frog.w >= this.dumptruck3.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
+
+        if(frog.y == 550){
+            if((frog.x <= this.bottomCar.w && frog.x >= this.bottomCar.x) || (frog.w <= this.bottomCar.w && frog.w >= this.bottomCar.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.bottomCar2.w && frog.x >= this.bottomCar2.x) || (frog.w <= this.bottomCar2.w && frog.w >= this.bottomCar2.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+            if((frog.x <= this.bottomCar3.w && frog.x >= this.bottomCar3.x) || (frog.w <= this.bottomCar3.w && frog.w >= this.bottomCar3.x)){
+                this.lives--
+                frog.x = 300
+                frog.y = 600
+            }
+        }
     
         //frog touches gator
 
